23415f62ab91e9c0f971cf0a8c1202b18d61c05b
1701manpt <thaiphuongnam1071@gmail.com>
2022-12-15 00:47:34 +0700

    .

--- src/configs/connectDb.js
+++ src/configs/connectDb.js
@@ -1,12 +1,26 @@
 require('dotenv').config()
 
+// const config = {
+//     "username": process.env.DB_USERNAME,
+//     "password": process.env.DB_PASSWORD,
+//     "database": process.env.DB_DATABASE,
+//     "host": process.env.DB_HOST,
+//     "dialect": "mssql",
+//     "port": 1433,
+//     // "port": 1433,
+//     // "dialectOptions": {
+//     //     "instanceName": process.env.SERVER
+//     // }
+// }
+
 const config = {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "host": "26.227.231.214",
     "dialect": "mssql",
-    "port": 49865,
+    "port": 1433,
+    // "port": 1433,
     // "dialectOptions": {
     //     "instanceName": process.env.SERVER
     // }
