4b74941aa7d167bd7fa1803005a808db92e4d2ec
1701manpt <thaiphuongnam1071@gmail.com>
2022-12-15 14:46:42 +0700

    .

--- src/configs/connectDb.js
+++ src/configs/connectDb.js
@@ -1,29 +1,15 @@
 require('dotenv').config()
 
-// const config = {
-//     "username": process.env.DB_USERNAME,
-//     "password": process.env.DB_PASSWORD,
-//     "database": process.env.DB_DATABASE,
-//     "host": process.env.DB_HOST,
-//     "dialect": "mssql",
-//     "port": 1433,
-//     // "port": 1433,
-//     // "dialectOptions": {
-//     //     "instanceName": process.env.SERVER
-//     // }
-// }
-
 const config = {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": "26.227.231.214",
+    // "host": process.env.DB_HOST,
     "dialect": "mssql",
     "port": 1433,
-    // "port": 1433,
-    // "dialectOptions": {
-    //     "instanceName": process.env.SERVER
-    // }
+    "dialectOptions": {
+        "instanceName": process.env.SERVER
+    }
 }
 
 module.exports = config
