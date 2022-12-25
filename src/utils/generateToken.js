a233aa18324e43e8b27dbd1e2d3e64bc640fa15b
1701manpt <thaiphuongnam1071@gmail.com>
2022-12-25 09:05:47 +0700

    9h00m25/12/2022

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 const generateToken = (user) => {
-    return jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '10s' });
+    return jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '1m' });
 }
 
 const generateRefreshToken = (user) => {
-    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '5m' });
 }
 
 const generateTokenRegister = (user) => {
-    return jwt.sign({ ...user }, process.env.REGISTER_TOKEN_SECRET, { expiresIn: '30s' });
+    return jwt.sign({ ...user }, process.env.REGISTER_TOKEN_SECRET, { expiresIn: '1m' });
 }
 
 module.exports = { generateToken, generateRefreshToken, generateTokenRegister }
